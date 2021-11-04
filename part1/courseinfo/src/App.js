b8fa28aa30d45136f7b47877d8dc6e27422f351a
drunkwretch <drunkwretch@gmail.com>
2021-11-05 00:37:37 +0800

    part1d conditional rendering

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,39 @@
 import React, { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
 
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
