e0ead1e34cc5b806cdb3a34b1346cc281afe8df8
drunkwretch <drunkwretch@gmail.com>
2021-11-04 10:58:23 +0800

    part1b javascript page pre-rendering

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,81 +1,10 @@
-import React from "react";
+import React from 'react'
 
-const Header = (props) => {
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  );
-};
+    <div>{counter}</div>
+  )
+}
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.name} {props.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part
-        name={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        name={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        name={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <h1>
-        Total of exercises:{" "}
-        {props.course.parts[0].exercises +
-          props.course.parts[1].exercises +
-          props.course.parts[2].exercises}
-      </h1>
-    </div>
-  );
-};
-
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
-};
-
-export default App;
+export default App
