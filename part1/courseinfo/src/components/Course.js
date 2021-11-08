f6f32f82fd76cc3f19683fd3abd011af47272973
drunkwretch <drunkwretch@gmail.com>
2021-11-08 10:30:58 +0800

    exercises2.1 courseinfo step6

---
+++ part1/courseinfo/src/components/Course.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { nanoid } from 'nanoid';
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (
+        <Part key={nanoid()} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  );
+};
+
+export default Course;
