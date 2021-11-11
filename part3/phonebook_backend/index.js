993962758a0b4f8d59ad826e2813694eabd89247
drunkwretch <drunkwretch@gmail.com>
2021-11-11 17:03:30 +0800

    exercises3.8 phonebook backend -- morgan custom token

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
+morgan.token("person", function (req, res, person) {
+  return JSON.stringify(req.body);
+});
 
 let persons = [
   {
