bdde7fff76ecb9723086406e4e0d7a9ae7f4218e
drunkwretch <drunkwretch@gmail.com>
2021-11-11 09:55:48 +0800

    exercises3.3 phonebook backend --/api/persons/:id

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -36,6 +36,15 @@ app.get("/info", (req, res) => {
     } people</p> <p>${new Date()}</p>`
   );
 });
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
