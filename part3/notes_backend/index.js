1953b7dbf69755c64616d83178ce640a37367d9c
drunkwretch <drunkwretch@gmail.com>
2021-11-16 10:59:22 +0800

    learning part3c frontend and backend integration

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,42 +1,55 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('build'))
+const cors = require("cors");
+require("dotenv").config();
+const Note = require("./models/note");
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
+app.use(express.static("build"));
+app.use(cors());
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
-app.post("/api/notes", (req, res) => {
-  const note = req.body;
-  console.log(note);
-  res.json(note);
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
