bdd53e043dcb658689aac346df7bf618b5d274bb
drunkwretch <drunkwretch@gmail.com>
2021-11-16 21:53:55 +0800

    learning part3d notes backend to production

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then((response) => {
+        setNotes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -13,15 +25,39 @@ const App = (props) => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    noteService
+      .create(noteObject)
+      .then((response) => {
+        setNotes(notes.concat(response.data));
+        setNewNote("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const handleNoteChange = (event) => {
     console.log("new note", event.target.value);
     setNewNote(event.target.value);
+  };
+
+  const toggleImportance = (id) => {
+    console.log("make", notes);
+    const note = notes.find((note) => note.id === id);
+    console.log("note", note);
+    const changedNote = { ...note, important: !note.important };
+    console.log("changedNote", changedNote);
+    noteService
+      .update(id, changedNote)
+      .then((response) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+        console.log("respinsedata", response.data);
+        console.log("newNotes", notes);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -36,12 +72,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportance(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -9,24 +9,36 @@ app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
 app.get("/api/notes/:id", (request, response) => {
   Note.findById(request.params.id).then((note) => {
@@ -34,12 +46,8 @@ app.get("/api/notes/:id", (request, response) => {
   });
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
 
   const note = new Note({
     content: body.content,
@@ -47,10 +55,47 @@ app.post("/api/notes", (request, response) => {
     date: new Date(),
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => savedNote.toJSON())
+    .then((savedAndFormattedNote) => {
+      response.json(savedAndFormattedNote);
+    })
+    .catch((error) => next(error));
 });
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  const note = new Note({
+    _id: id,
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  Note.findByIdAndUpdate(id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
