acb122d10dc94c4eacf483395149e0dcf4e1a47a
drunkwretch <drunkwretch@gmail.com>
2021-11-16 14:13:58 +0800

    learning part3c mongodb error handling

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -30,16 +30,20 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(req.params.id);
-  const person = Person.findById(id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
