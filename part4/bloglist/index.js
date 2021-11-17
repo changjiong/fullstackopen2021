ff2a5497005dd49b65898cb887d0a6503d87bdc2
drunkwretch <drunkwretch@gmail.com>
2021-11-17 23:55:53 +0800

    exercises4.8 bloglist tests --step1

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,5 +1,3 @@
-const resolve = require('path').resolve
-console.log('__dirname : ' + __dirname)
 const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
@@ -7,5 +5,5 @@ const config = require('./utils/config')
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+    console.log(`Server running on port ${config.PORT}`)
 })
---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+}, 60000)
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}, 10000)
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(2)
+})
+
+test('the first blog is about HTTP methods', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].content).toBe('HTML is easy')
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a specific blog can be viewed', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToView = blogsAtStart[0]
+
+    const resultBlog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
+    expect(resultBlog.body).toEqual(processedBlogToView)
+})
+test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+    )
+
+    const contents = blogsAtEnd.map(r => r.content)
+
+    expect(contents).not.toContain(blogToDelete.content)
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(n => n.content)
+    expect(contents).toContain(
+        'async/await simplifies making async calls'
+    )
+})
+
+test('blog without content is not added', async () => {
+    const newBlog = {
+        important: true
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+
+},60000)
+
+afterAll(() => {
+    mongoose.connection.close()
+})
