37352626e0180623503a8a494fababc64e259fe2
drunkwretch <drunkwretch@gmail.com>
2021-11-18 22:15:09 +0800

    exercises4.18  bloglist expansions --step6 token authentication

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const mongoUrl = config.MONGODB_URI
 
@@ -20,5 +21,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -7,6 +8,16 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 
 })
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+
+    return null
+}
+
 
 blogsRouter.post('/', async (request, response) => {
     if (!request.body.likes) {
@@ -17,8 +28,14 @@ blogsRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: 'title and url are required' })
     }
 
-    const userId = request.body.userId
-    const user = await User.findById(userId)
+
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     if (!user) {
         return response.status(400).json({ error: 'user not found' })
     }
