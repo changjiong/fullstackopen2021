29fe69f7445064cc466950d374f1027bfb5109fe
drunkwretch <drunkwretch@gmail.com>
2021-12-02 20:34:29 +0800

    learning part5b ref

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
@@ -37,6 +37,7 @@ const App = () => {
     }, [])
 
     const addNote = (noteObject) => {
+        noteFormRef.current.toggleVisibility()
         noteService.create(noteObject).then((returnedNote) => {
             setNotes(notes.concat(returnedNote))
         })
@@ -100,9 +101,10 @@ const App = () => {
             />
         </Togglable>
     )
+    const noteFormRef = useRef()
 
     const noteForm = () => (
-        <Togglable buttonLabel='new note'>
+        <Togglable buttonLabel='new note' ref={noteFormRef}>
             <NoteForm createNote={addNote} />
         </Togglable>
     )
